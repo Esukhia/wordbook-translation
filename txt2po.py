d630a0b64db431afcd45e3905fa9ba75a41e8f4c
bdrc <n.gawangtrinley@gmail.com>
2021-08-05 00:04:44 +0800

    uuids

--- txt2po.py
+++ txt2po.py
@@ -1,6 +1,10 @@
 from pathlib import Path
-
 import polib
+from uuid import uuid4
+
+
+def get_unique_id():
+    return uuid4().hex
 
 
 def find_sents(infile):
@@ -20,7 +24,7 @@ def create_po(infile, outfile):
     for sent in sents:
         entry = polib.POEntry(
                 # other arguments are: msgid, msgid, msgtxt, comment, tcomment            
-                msgid=sent,
+                msgid=get_unique_id(),
                 msgstr=sent
             )
         po.append(entry)
@@ -31,5 +35,7 @@ def create_po(infile, outfile):
 if __name__ == '__main__':
     input = "wordbook.txt"
     output = "wordbook.po"
+
+    print(get_unique_id())
 
     create_po(input, output)
